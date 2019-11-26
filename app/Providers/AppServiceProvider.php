e7713ff9bc29844d0c8d887234079dea27443bdf
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2019-11-27 00:54:26 +0600

    Binds document repo interface to its implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\DocumentRepository;
+use App\Repositories\DocumentRepositoryEloquent;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(DocumentRepository::class, DocumentRepositoryEloquent::class);
     }
 
     /**
