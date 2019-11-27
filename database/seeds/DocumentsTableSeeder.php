9b44e0448f8611e0cc0ca142d0c811362f373559
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2019-11-27 19:55:26 +0600

    Adds document table seeder

---
+++ database/seeds/DocumentsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Document;
+use Illuminate\Database\Seeder;
+
+class DocumentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Document::class, 10)->create();
+    }
+}
