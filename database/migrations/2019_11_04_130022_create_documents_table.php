9830169ef1aed9882bb4d4b590cca6acfd310c74
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2019-11-27 19:53:26 +0600

    Adds document migration

---
+++ database/migrations/2019_11_04_130022_create_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('status', ['draft',  'published']);
+            $table->json('payload');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
