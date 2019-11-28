790665ed012cbf4d03a932b60903fdcad72e2038
Mirlan Jelamanov <mirlan.jelamanov@gmail.com>
2019-11-28 21:40:15 +0600

    Adds document entity

---
+++ app/Document.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use App\Enums\DocumentStatus;
+use DateTime;
+use Illuminate\Database\Eloquent\Model;
+use MadWeb\Enum\EnumCastable;
+
+class Document extends Model
+{
+    use EnumCastable;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['status', 'payload'];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'status' => DocumentStatus::class,
+        'payload' => 'array',
+        self::CREATED_AT => 'date:' . DateTime::ATOM,
+        self::UPDATED_AT => 'date:' . DateTime::ATOM,
+    ];
+}
